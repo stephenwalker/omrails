09747e5b15c495fa3875abed5e5ea4a19825dc39
Stephen Walker <skywalkernyc@gmail.com>
2013-06-06 15:51:40 -0400

    Add Pagination

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Omrails::Application.routes.draw do 
+  get "users/show"
+
   resources :pins
 
 
   devise_for :views
 
   devise_for :users
+  match 'users/:id' => 'users#show', as: :user
 
   get 'about' => 'pages#about'
 
